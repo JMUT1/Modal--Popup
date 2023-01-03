a44d4306d03ee7ee72cf3c23a5e710069dc6cfd9
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-03 16:51:46 -0600

    cursor pointer

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ openBtn.addEventListener("click", openModal)
 function openModal(){
     modal.style.display = "grid"
 }
+
+closeBtn.addEventListener("click", closeModal)
+
+function closeModal(){
+    modal.style.display = "none"
+}
